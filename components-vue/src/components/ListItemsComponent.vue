de5928811de8937163ed364baeefc8f04a2d868d
William Felicio Freire <william.freire@credisis.local>
2019-12-23 15:30:41 -0300

    Props e $emit - passar dado entre components

---
+++ components-vue/src/components/ListItemsComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+    <h2>{{title}}</h2>
+
+    <ul>
+      <li v-for="(item, index) in items" :key="index">{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      required: true,
+      type: String,
+      default: ""
+    },
+    items: {
+      required: true,
+      type: Array,
+      default: []
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ components-vue/src/components/TasksComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app">
+    <h2>{{title}}</h2>
+
+    <input type="text" v-model="task" />
+    <button @click="callDad">Chamar Pai</button>
+
+    <ul>
+      <li v-for="(item, index) in items" :key="index">{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["items", "title"],
+  data() {
+    return {
+        task: ''
+    };
+  },
+  methods: {
+    callDad() {
+      this.$emit("callDad", this.task);
+
+      this.task = ''
+
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
