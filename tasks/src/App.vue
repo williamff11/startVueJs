3c19c79fb27a58b6b5bd365ca3e7fe3088d5ea9c
William Felicio Freire <william.freire@credisis.local>
2019-12-23 16:39:19 -0300

    crud with vuejs

---
+++ tasks/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" class="container">
+    
+    <list-tasks></list-tasks>
+  </div>
+</template>
+
+<script>
+import ListTasks from  './components/Tasks/ListTasks'
+
+export default {
+  name: 'app',
+  components: {
+    ListTasks
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ tasks/src/components/Tasks/ListTasks.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h2>{{title}}</h2>
+
+    <form class="form form-inline" @submit.prevent="save()">
+      <input type="text" placeholder="Nome Tarefa" class="form-control" v-model="task.name" />
+      <button type="submit" class="btn btn-success">Enviar</button>
+    </form>
+
+    <table class="table table-dark">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nome</th>
+          <th width="180px">Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>{{task.id}}</td>
+          <td>{{task.name}}</td>
+          <td>
+            <a href class="btn btn-info">Editar</a>
+            <a href class="btn btn-danger">Deletar</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Listagem de Tarefas",
+      tasks: [],
+      task: {
+        id: "",
+        name: ""
+      }
+    };
+  },
+  methods: {
+    save() {
+      this.task.id = this.tasks.length + 1;
+      this.tasks.push(this.task);
+
+      this.task = {
+        id: "",
+        name: ""
+      };
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  margin: 20px 0;
+}
+</style>
